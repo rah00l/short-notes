bca035e883147bec68bb5428c605204b39094517
Rahul Patil <rahulpatil2387@gmail.com>
2016-01-17 00:11:13 +0530

    Dev-03 - Add migration file of post and comment

---
+++ db/migrate/20160116140746_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.integer :user_id
+      t.boolean :publish
+
+      t.timestamps null: false
+    end
+  end
+end
