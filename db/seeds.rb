09730b03681a11ccde6f7dcd03394c4cc38b3b8a
Rahul Patil <rahulpatil2387@gmail.com>
2016-01-17 00:46:37 +0530

    Dev-03 - Update seed to have a initial single dummy user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rake db:seed
+# (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Creating admin user to start application with one user
+admin_user = User.find_by_email('rahulpatil2387@gmail.com')
+admin_user.destroy if admin_user
+User.create! email: 'rahulpatil2387@gmail.com',
+             password: 'rah00l2387',
+             password_confirmation: 'rah00l2387',
+             admin: true
