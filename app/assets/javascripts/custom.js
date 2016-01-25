10635ef5d48ed0a4eb1b843585755f01f75035c7
Rahul Patil <rahulpatil2387@gmail.com>
2016-01-26 03:52:54 +0530

    Dev-08 - Add custom js file for adding js code, currently add to get recent posts asynch

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,20 @@
+  // Js file to write custom application-specific javascript.
+
+// Following JS code for refreshing DIV content which have recent posts:
+window.setInterval(pick_recent_posts, 20000);
+function pick_recent_posts() {
+  $.ajax({
+    url: '/posts/recent_posts',
+    type: 'get',
+    dataType:'script',
+    data: { },
+    success: function(data){
+      // $(".response").fadeOut(600, function ()
+      //  {
+      //     $(".response").html(data)
+      //   });
+      // $(".response").fadeIn(600);
+    }
+  });
+  return false;
+}
