4605e1db19225354b93fea5c598fc636af42a623
Rahul Patil <rahulpatil2387@gmail.com>
2016-01-26 03:08:45 +0530

    Dev-07 - Add helper method to get image name

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+  # action to pick randum image from bunch of images
+  def pick_image_name(size)
+    ["#{size}_1.png", "#{size}_2.jpg", "#{size}_3.png", "#{size}_4.jpg", "#{size}_5.png", "#{size}_6.jpg", "#{size}_7.jpg", "#{size}_8.jpg"].sample
+  end
 end
