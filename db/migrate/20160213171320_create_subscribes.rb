44aa11f39d9926508b1384763927e66c5eea82e8
Rahul Patil <rahulpatil2387@gmail.com>
2016-02-14 03:04:23 +0530

    Dev-08 - Remove extra character

--- db/migrate/20160213171320_create_subscribes.rb
+++ db/migrate/20160213171320_create_subscribes.rb
@@ -2,7 +2,7 @@ class CreateSubscribes < ActiveRecord::Migration
   def change
     create_table :subscribes do |t|
       t.string :email
-      t.boolean :confirm, default: false`
+      t.boolean :confirm, default: false
 
       t.timestamps null: false
     end
