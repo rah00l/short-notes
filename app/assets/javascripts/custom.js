a0bc864679a538016ca87dd2e5ec9dad55189ee9
Rahul Patil <rahulpatil2387@gmail.com>
2016-02-14 02:47:36 +0530

    Dev-09 - Add js for validate email of subscription

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -18,3 +18,31 @@ function pick_recent_posts() {
   });
   return false;
 }
+
+$(document).ready(function() {
+   $('#btnValidate').click(function(e) {
+        var sEmail = $('#email').val();
+        if ($.trim(sEmail).length == 0) {
+            alert('Please enter valid email address');
+            e.preventDefault();
+        }
+        if (validateEmail(sEmail)) {
+            //alert('Email is valid');
+        }
+        else {
+            //alert('Invalid Email Address');
+            e.preventDefault();
+        }
+    });
+   $('.flash').fadeOut(3000);
+});
+
+function validateEmail(sEmail) {
+    var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (filter.test(sEmail)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
