6eceefba331a6e512c52de6a243ebdba47af0d72
Rahul Patil <rahulpatil2387@gmail.com>
2016-02-14 02:41:57 +0530

    Dev-09 - Add subscribe modeal and it's all dependent files

---
+++ db/migrate/20160213171320_create_subscribes.rb
@@ -0,0 +1,10 @@
+class CreateSubscribes < ActiveRecord::Migration
+  def change
+    create_table :subscribes do |t|
+      t.string :email
+      t.boolean :confirm, default: false`
+
+      t.timestamps null: false
+    end
+  end
+end
