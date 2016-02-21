0cf140d7b50f688abc4fd05b031b75409ebe1302
Rahul Patil <rahulpatil2387@gmail.com>
2016-02-21 13:28:49 +0530

    Dev-09 - Add mail templates and update actions

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,17 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true
 
+  after_create :notify_post_created
+
   # mount the uploader
   mount_uploader :image, ImageUploader
 
   #for order default scope
   default_scope { order(created_at: :desc) }
+
+  private
+  # Intimate subscribed users that new post has been created.
+  def notify_post_created
+    PostMailer.post_created(self).deliver_now
+  end
 end
